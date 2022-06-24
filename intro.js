48ba4e36d3728605363f34df5ca4a5d91a4dcb75
ChinonsoOgakwu <105208406+ChinonsoOgakwu@users.noreply.github.com>
2022-06-24 16:44:45 +0100

    Assignment done

---
+++ intro.js
@@ -0,0 +1,5 @@
+let myName = "Chinonso Ogakwu";
+let myHeight = "190 cm"
+let myCountry = "Nigeria"
+
+document.write("My name is: " + myName + "<br>" + "My height: " + myHeight + "<br>" + "My country of origin is: " + myCountry)
